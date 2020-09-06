5a654b6b74cb0cd83d9a70b78337e471c13c2fce
bertei <70592221+bertei@users.noreply.github.com>
2020-09-06 14:58:15 -0300

    c1

---
+++ CAI_Clase1/Persona.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Clase1
+{
+    class Persona
+    {
+        //variables
+        private string _nombre;
+        private int _edad;
+        private string _apellido;
+
+        //propiedades de la clase
+        private int cantidadPiernas;
+
+        public string Nombre
+        {
+            get
+            {
+                return this._nombre;
+            }
+            set
+            {
+                this._nombre = value;
+            }
+        }
+
+        public string NombreCompleto
+        {
+            get
+            {
+                return this._nombre + " " + this._apellido;
+            }
+        }
+
+        public int Edad
+        {
+            get { return _edad; }
+            set { _edad = value; }
+        }
+
+        //constructor sin parametros
+        public Persona()
+        {
+            this.cantidadPiernas = 2;
+        }
+
+        //constructor con tres parametros
+        public Persona(string nombre, string apellido, int edad)
+        {
+            this.cantidadPiernas = 2;
+            this._edad = edad;
+            this._nombre = nombre;
+        }
+
+        //constructor con dos parametros
+        public Persona(string nombre, int edad)
+        {
+            this.cantidadPiernas = 2;
+            this._edad = edad;
+            this._nombre = nombre;
+        }
+
+        public Persona(int nombre, string edad)
+        {
+
+        }
+
+        //metodos de la clase
+        public void SumarEdad(int algo)
+        {
+            this._edad += algo;
+        }
+
+        public int SumarUnoAlaEdad()
+        {
+            this._edad = this._edad + 1;
+            return this._edad;
+        }
+
+        public void Correr()
+        {
+            if(this.cantidadPiernas == 2)
+            {
+                Console.WriteLine("Puedo correr.");
+            }
+            else
+            {
+                Console.WriteLine("No puedo correr.");
+            }
+        }
+    }
+}
---
+++ CAI_Clase1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_Clase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instancio una persona en una variable de tipo persona
+            Persona p1 = new Persona();
+
+            //setteo los atributos mediante su propiedad (encapsulamiento)
+            p1.Nombre = "Jose";
+            p1.Edad = 30;
+
+            Console.WriteLine(p1.Nombre);
+            Console.WriteLine(p1.Edad.ToString()); //get
+
+            //ejecuto un metodo de este objeto
+            p1.SumarEdad(5);
+
+            Console.WriteLine(p1.Edad.ToString());
+
+            //instancio otro objeto de tipo persona en la variable p2 de tipo Persona
+            Persona p2 = new Persona("Juan", 33);
+            Console.WriteLine(p2.Nombre);
+            Console.WriteLine(p2.Edad.ToString());
+            //declaro una variable de tipo persona pero no tiene instancia p3 es null
+            Persona p3;
+        }
+    }
+}
